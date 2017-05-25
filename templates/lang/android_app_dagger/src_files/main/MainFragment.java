1bf86635ea47fd77705c3ff05f6455c205eace38
Geoff Hackett <ghackett@gmail.com>
2017-05-25 18:49:39 -0400

    add butterknife to default dagger android app project (probably should rename it)

--- templates/lang/android_app_dagger/src_files/main/MainFragment.java
+++ templates/lang/android_app_dagger/src_files/main/MainFragment.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import {{ .Group }}.R;
 import {{ .Group }}.base.BaseFragment;
 
 import javax.annotation.Nullable;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainFragment extends BaseFragment {
+
+  @BindView(R.id.tv_hello_world) TextView mTextView;
 
   @Nullable
   @Override
@@ -19,5 +25,12 @@ public class MainFragment extends BaseFragment {
       @Nullable ViewGroup container,
       Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_main, container, false);
+  }
+
+
+  @Override
+  public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+    ButterKnife.bind(this, view);
+    mTextView.setText("Hello Earth!");
   }
 }
