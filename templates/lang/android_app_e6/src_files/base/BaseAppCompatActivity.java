a2a588a727a81b7a81cc29a2549eb352107a55b0
Geoff Hackett <ghackett@gmail.com>
2017-06-21 16:01:16 -0400

    add disposables to base activity

--- templates/lang/android_app_e6/src_files/base/BaseAppCompatActivity.java
+++ templates/lang/android_app_e6/src_files/base/BaseAppCompatActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.episode6.hackit.android.inject.context.qualifier.ForContextScope;
+import com.episode6.hackit.disposable.Disposable;
+import com.episode6.hackit.pausable.Pausable;
+import com.episode6.hackit.pausable.PausableDisposableManager;
+
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
@@ -15,6 +20,7 @@ import dagger.android.HasFragmentInjector;
 public class BaseAppCompatActivity extends AppCompatActivity implements HasFragmentInjector {
 
   @Inject DispatchingAndroidInjector<Fragment> mFragmentInjector;
+  @Inject @ForContextScope PausableDisposableManager mDisposables;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,7 +29,33 @@ public class BaseAppCompatActivity extends AppCompatActivity implements HasFragm
   }
 
   @Override
+  protected void onResume() {
+    super.onResume();
+    mDisposables.resume();
+  }
+
+  @Override
+  protected void onPause() {
+    mDisposables.pause();
+    super.onPause();
+  }
+
+  @Override
+  protected void onDestroy() {
+    mDisposables.dispose();
+    super.onDestroy();
+  }
+
+  @Override
   public AndroidInjector<Fragment> fragmentInjector() {
     return mFragmentInjector;
+  }
+
+  protected void registerDisposable(Disposable disposable) {
+    mDisposables.addDisposable(disposable);
+  }
+
+  protected void registerPausable(Pausable pausable) {
+    mDisposables.addPausable(pausable);
   }
 }
--- templates/lang/android_app_e6/src_files/test/ut/base/BaseAppCompatActivityTest.java
+++ templates/lang/android_app_e6/src_files/test/ut/base/BaseAppCompatActivityTest.java
@@ -1,6 +1,11 @@
 package {{ .Group }}.base;
 
+import com.episode6.hackit.android.inject.context.qualifier.ForContextScope;
+import com.episode6.hackit.disposable.Disposable;
 import com.episode6.hackit.mockspresso.Mockspresso;
+import com.episode6.hackit.mockspresso.annotation.Unmapped;
+import com.episode6.hackit.pausable.Pausable;
+import com.episode6.hackit.pausable.PausableDisposableManager;
 
 import {{ .Group }}.BuildConfig;
 import {{ .Group }}.BuildMockspresso;
@@ -9,12 +14,15 @@ import {{ .Group }}.app.MockspressoTestApp;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.inOrder;
 
 /**
  * Tests {@link BaseAppCompatActivity}
@@ -26,14 +34,32 @@ public class BaseAppCompatActivityTest {
   @Rule public final Mockspresso.Rule mMockspresso = BuildMockspresso.forRobolectric()
       .buildRule();
 
+  @Mock @ForContextScope PausableDisposableManager mDisposables;
+
+  @Mock @Unmapped Disposable mDisposable;
+  @Mock @Unmapped Pausable mPausable;
+
   @Test
   public void sanityCheck() {
+    InOrder inOrder = inOrder(mDisposables);
+
     ActivityController<BaseAppCompatActivity> controller = Robolectric.buildActivity(BaseAppCompatActivity.class)
         .setup();
     BaseAppCompatActivity activity = controller.get();
 
+    inOrder.verify(mDisposables).resume();
     assertThat(activity.fragmentInjector()).isNotNull();
 
-    controller.pause().stop().destroy();
+    activity.registerDisposable(mDisposable);
+    inOrder.verify(mDisposables).addDisposable(mDisposable);
+
+    activity.registerPausable(mPausable);
+    inOrder.verify(mDisposables).addPausable(mPausable);
+
+    controller.pause();
+    inOrder.verify(mDisposables).pause();
+
+    controller.stop().destroy();
+    inOrder.verify(mDisposables).dispose();
   }
 }
