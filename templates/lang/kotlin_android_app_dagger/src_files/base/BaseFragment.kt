6f2f13754c9bf96f85e4305c9898cdccf11571b6
Geoff Hackett <ghackett@gmail.com>
2018-06-30 17:39:40 -0400

    kaad - fix base fragment imports

--- templates/lang/kotlin_android_app_dagger/src_files/base/BaseFragment.kt
+++ templates/lang/kotlin_android_app_dagger/src_files/base/BaseFragment.kt
@@ -1,17 +1,5 @@
 package {{ .Group }}.base
 
-import android.app.Activity;
-import android.content.Context;
-import android.os.Build;
-import android.support.v4.app.Fragment;
-
-import javax.inject.Inject;
-
-import dagger.android.AndroidInjector;
-import dagger.android.DispatchingAndroidInjector;
-import dagger.android.support.AndroidSupportInjection;
-import dagger.android.support.HasSupportFragmentInjector;
-
 import android.app.Activity
 import android.content.Context
 import android.os.Build
